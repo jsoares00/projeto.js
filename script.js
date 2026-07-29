95ca3f24bb25409ace8f931fe11043c400c40f7a
jsoares00 <joassoares22@gmail.com>
2026-07-29 20:57:14 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -1,182 +1,122 @@
-
-// C >> Create  >> Cadastrar
-// R   >> Read   >> Ler
-// U   >>  Update  >> alterar/atualizar/editar/fuçar/mudar
-// D   >>  Delete  >> Apagar/deletar/excluir
-
-// ctrl + ;
-
-// const nomes = []
-// const alturas = []
-
-
-
-
-// console.log(dino);
-
-let dinos = []
-
 function salvarDados(){
-    localStorage.setItem('dinos', JSON.stringify(dinos))
-    
-    // let texto = JSON.stringify(dinos)
-    // localStorage.setItem('dinos', texto)
+    localStorage.setItem('carros', JSON.stringify(carros))
 }
 
 function carregarDados(){
-    dinos = JSON.parse(localStorage.getItem('dinos')) || []
-    
-    // let textoLido = localStorage.getItem('dinos')
-    // dinos = JSON.parse(textoLido)
+    carros = JSON.parse(localStorage.getItem('carros')) || []
 }
 
-function cadastrarDino() {
+let carros=[]
 
-    carregarDados()
+function cadastrarCarro(){
+carregarDados()    
 
-    const novoDino = {
-        id: Date.now(),
-        nome: document.getElementById('input-nome').value,
-        altura: Number(document.getElementById("input-altura").value),
-        cor: document.getElementById("input-cor").value,
-        custo: Number(document.getElementById("input-custo").value),
+    const novoCar = {
+    nome: document.getElementById('nome').value,
+    ano: Number(document.getElementById("ano").value),
+    cor: document.getElementById("cor").value,
+    custo: Number(document.getElementById("custo").value),
+    placa: document.getElementById('placa').value,
     }
-    dinos.push(novoDino)
+   
+carros.push(novoCar)
 
-    console.log(dinos);
+console.log(carros)
 
-    limparFormulario()
-    mostrarTodos()
-
-    salvarDados()
+salvarDados()
+limparFormulario()
+mostrarTodos()
 }
-
 function limparFormulario() {
-    document.getElementById('input-nome').value = ''
-    document.getElementById('input-altura').value = ''
-    document.getElementById('input-cor').value = ''
-    document.getElementById('input-custo').value = ''
-    document.getElementById('input-id').value = ''
-
-    document.getElementById('input-nome').focus()
+    document.getElementById('nome').value = ''
+    document.getElementById('ano').value = ''
+    document.getElementById('cor').value = ''
+    document.getElementById('custo').value = ''
+    document.getElementById('placa').value = ''
 }
-
 function mostrarTodos(){
-    document.getElementById('painel-dinos').innerHTML = '' 
+carregarDados()
 
-    for(let i=0; i<dinos.length; i++){
-        // alert(dinos[i].nome)
-        document.getElementById('painel-dinos').innerHTML += 
-        `<div class="card-dino">
-            <h2>${dinos[i].nome}</h2>
-            <p>Altura: ${dinos[i].altura}</p>
-            <p>Cor: ${dinos[i].cor}</p>
-            <p>Custo: ${dinos[i].custo}</p>
-            <p>${dinos[i].id}</p>
-        
+    for(let i=0; i<carros.length; i++){
+        document.getElementById('painel-Resultado').innerHTML += 
+        `<div>
+            <h2>${carros[i].nome}</h2>
+            <p>ano: ${carros[i].ano}</p>
+            <p>Cor: ${carros[i].cor}</p>
+            <p>Divida: R$${carros[i].custo}</p>
+            <p>Placa: ${carros[i].placa}</p>
         </div>
         ` 
     }
 }
-
-function testar() {
-    
-    // window.location.href = 'teste.html'
-
-    carregarDados()
-
-    localStorage.setItem('teste', 45)
-
-    let testeDeLeitura = localStorage.getItem('teste')
-    console.log(testeDeLeitura);
-    
-
-    dinos = [
-        {
-            id: 1718324500001,
-            nome: "Rex",
-            altura: 6.1,
-            cor: "#8B4513", // Marrom
-            custo: 150000
-        },
-        {
-            id: 1718324500002,
-            nome: "Velo",
-            altura: 2.0,
-            cor: "#2E8B57", // Verde escuro
-            custo: 85000
-        },
-        {
-            id: 1718324500003,
-            nome: "Trike",
-            altura: 3.0,
-            cor: "#708090", // Cinza
-            custo: 120000
-        },
-        {
-            id: 1718324500004,
-            nome: "Braqui",
-            altura: 13.0,
-            cor: "#556B2F", // Verde oliva
-            custo: 250000
-        },
-        {
-            id: 1718324500005,
-            nome: "Ptero",
-            altura: 1.5,
-            cor: "#4682B4", // Azul aço
-            custo: 95000
+function retirarCarro(){
+    let placa = document.getElementById('pesquisa').value
+    for(let i = 0; i<carros.length; i++){
+        if(placa == carros[i].placa ){
+            console.log(carros[i]);
+            carros.splice(i, 1);
+            console.log(i);
         }
-    ]
-
-    console.log(dinos);
- mostrarTodos()
+    }
+    salvarDados()
+    mostrarTodos()
+    limparFormulario()
+    
 }
 
+function testar(){
+ carregarDados()
+
+ document.getElementById('painel-Resultado').innerHTML += 
+        `<div>
+            <h2>Civic</h2>
+            <p>ano: 2022</p>
+            <p>Cor: Preta</p>
+            <p>Divida: 500</p>
+            <p>Placa: 7FA7WS2</p>
+        </div>
+        `+
+        `<div>
+            <h2>Corolla</h2>
+            <p>ano: 2021</p>
+            <p>Cor: Prata</p>
+            <p>Divida: 5000</p>
+            <p>Placa: 7M4S1T2</p>
+        </div>
+        ` +
+        `<div>
+            <h2>Onix</h2>
+            <p>ano: 2020</p>
+            <p>Cor: Branco</p>
+            <p>Divida: 300</p>
+            <p>Placa: FH8S9G</p>
+        </div>
+        `+
+        `<div>
+            <h2>Gol G3</h2>
+            <p>ano: 2016</p>
+            <p>Cor: Preta</p>
+            <p>Divida: 200</p>
+            <p>Placa: 72DTE8</p>
+        </div>
+        ` 
+console.log(carros)
+
+mostrarTodos()
+}
 function pesquisar(){
-    let nomeProcurado = document.getElementById('input-nome').value
-
-    for(let i = 0; i<dinos.length; i++){
-        if(nomeProcurado == dinos[i].nome ){
-            console.log(dinos[i]);
-            document.getElementById('input-altura').value = dinos[i].altura
-            document.getElementById('input-cor').value = dinos[i].cor
-            document.getElementById('input-custo').value = dinos[i].custo
-            document.getElementById('input-id').value = dinos[i].id
-            console.log(i);
-        }
-        
-    }
-
-}
-
-function salvarDino(){
-    let id = Number(document.getElementById('input-id').value)
-
-    for(let i = 0; i<dinos.length; i++){
-        if(id == dinos[i].id ){
-            console.log(dinos[i]);
-            dinos[i].altura = document.getElementById('input-altura').value 
-            dinos[i].cor = document.getElementById('input-cor').value 
-            dinos[i].custo = document.getElementById('input-custo').value 
-            dinos[i].id = document.getElementById('input-id').value 
-            console.log(i);
+    let placa = document.getElementById('pesquisa').value
+    for(let i = 0; i<carros.length; i++){
+        if(placa == carros[i].placa ){
+           document.getElementById('resultadoPes').innerHTML = 
+        `<div>
+            <h2>${carros[i].nome}</h2>
+            <p>ano: ${carros[i].ano}</p>
+            <p>Cor: ${carros[i].cor}</p>
+            <p>Divida: R$${carros[i].custo}</p>
+            <p>Placa: ${carros[i].placa}</p>
+        </div>
+        ` 
         }
     }
-    mostrarTodos()
-    limparFormulario()
-}
-
-function excluirDino(){
-    let id = Number(document.getElementById('input-id').value)
-
-    for(let i = 0; i<dinos.length; i++){
-        if(id == dinos[i].id ){
-            console.log(dinos[i]);
-            dinos.splice(i, 1)
-            console.log(i);
-        }
-    }
-    mostrarTodos()
-    limparFormulario()
 }
